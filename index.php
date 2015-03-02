fdbc29707e780af6fb806101d4c0049eed0e36d4
Pascal Herbert <pascal.herbert@gmail.com>
2015-03-02 20:47:29 +0100

    use local jquery

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 	</div><!-- #wrap -->
 </body>
 	<!-- JavaScript Files Go Here -->
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/jquery-2.1.3.min.js"></script>
 	<script>
 		
 		add_task(); // Call the add_task function
